21cf0953dfd6278386f8cd242fc53bc06572117e
Carin Meier <cmeier@gigasquidsoftware.com>
2013-01-31 22:02:11 -0500

    in progress goals

--- src/clj_drone/goals.clj
+++ src/clj_drone/goals.clj
@@ -1,12 +1,25 @@
 (ns clj-drone.goals)
 
-(defmacro def-belief-action [bname belief-str belief-fn action-str action-fn]
+(def current-belief (atom "None"))
+(def current-goal (atom "None"))
+
+(defmacro def-belief-action [bname belief-str belief-fn action-fn]
   `(def ~bname { :belief-str ~belief-str
                  :belief-fn ~belief-fn
-                 :action-str ~action-str
                  :action-fn ~action-fn}))
 
 (defmacro def-goal [gname goal-str goal-fn belief-actions]
   `(def ~gname { :goal-str ~goal-str
                  :goal-fn ~goal-fn
                  :belief-actions ~belief-actions}))
+
+(defn eval-belief-action [{:keys [belief-str belief-fn  action-fn]}]
+  (when (belief-fn)
+    (reset! current-belief belief-str)
+    (action-fn)))
+
+(defn eval-goal [{:keys [goal-str goal-fn belief-actions]}]
+  (when (goal-fn)
+    (reset! current-goal goal-str)
+    (map eval-belief-action belief-actions)
+    :goal-reached))
--- test/clj_drone/goals_test.clj
+++ test/clj_drone/goals_test.clj
@@ -3,22 +3,42 @@
         midje.sweet
         clj-drone.goals))
 
+(defn belief-fn1 [] (= 1 1))
+(defn belief-fn2 [] (= 1 2))
+(defn action-fn1 [] (str "My first action fn"))
+(def-belief-action ba1 "belief 1" belief-fn1 action-fn1)
+(def-belief-action ba2 "belief 2" belief-fn2 action-fn1)
+(defn goal-fn1 [] (= 2 2))
+(def-goal g1 "goal 1" goal-fn1 [ba1 ba2])
+
 (fact "about def-belief-action"
   (let [ belief-str "I am on the ground"
          belief-fn #(+ 1 1)
-         action-str "Multipy"
          action-fn #(* 2 1)]
-    (def-belief-action b-ground belief-str belief-fn action-str action-fn) => anything
+    (def-belief-action b-ground belief-str belief-fn action-fn) => anything
     (b-ground :belief-str "I am on the ground")
     ((b-ground :belief-fn)) => 2
-    (b-ground :action-str "Multiply")
     ((b-ground :action-fn)) => 2))
 
 (fact "about def-goal"
   (let [ goal-str "Be Happy"
          goal-fn #(str ":)")
-         smile (def-belief-action ba-sad "sad" #(str "Sad") "smile" #(str "Smile"))]
+         smile (def-belief-action ba-sad "sad" #(str "Sad")  #(str "Smile"))]
     (def-goal be-happy goal-str goal-fn [smile]) => anything
-  (be-happy :goal-str) => "Be Happy"
-  ((be-happy :goal-fn)) => ":)"
-  (first (be-happy :belief-actions)) => smile))
+    ((be-happy :goal-fn)) => ":)"
+    (first (be-happy :belief-actions)) => smile))
+
+(fact "eval-belief-action will execute the action if the belief-fn is true"
+  (eval-belief-action ba1) => "My first action fn"
+  @current-belief => "belief 1")
+
+(fact "eval-belief-action will not execute action if the belief-fn is false"
+  (eval-belief-action ba2) => nil
+  @current-belief => "None"
+  (against-background (before :facts (reset! current-belief "None"))))
+
+(fact "eval-goal will execute the action if the goal is true"
+  (eval-goal g1) => "Goal Reached"
+  @current-belief => "belief 1"
+  @current-goal => "goal 1")
+
