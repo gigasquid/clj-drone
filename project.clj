581c628397b4a07fdd4aac38403e5d77626d2134
Carin Meier <cmeier@gigasquidsoftware.com>
2013-07-12 12:03:20 -0400

    bumping version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-drone "0.1.5"
+(defproject clj-drone "0.1.6"
   :description "Clojure Control for the AR Drone"
   :url "https://github.com/gigasquid/clj-drone"
   :license {:name "Eclipse Public License"
