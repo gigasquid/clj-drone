ff33d0138f01fe18fccb0472fd6adc7fa9c1f8a4
Carin Meier <cmeier@gigasquidsoftware.com>
2013-08-20 16:47:41 -0400

    basic working with multi-drone

--- examples/simple.clj
+++ examples/simple.clj
@@ -3,7 +3,7 @@
 
 (drone-initialize)
 ;Use ip and port for non-standard drone ip/port
-;(initialize ip port)
+;(drone-initialize :default ip atport navdataport)
 (drone :take-off)
 (Thread/sleep 10000)
 (drone :land)
